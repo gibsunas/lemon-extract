f8b6cac11c3b605292ad6dcf4c2b56f837f88555
James Gibson <james.the.nodester@gmail.com>
2020-08-11 13:10:04 -0600

    More publishing fixes

--- scripts/publishing/prepare.ts
+++ scripts/publishing/prepare.ts
@@ -1,5 +1,5 @@
 // Adapted from https://stackoverflow.com/a/52177090
-
+const path = require("path");
 const fs = require("fs");
 
 // DO NOT DELETE THIS FILE
@@ -7,17 +7,18 @@ const fs = require("fs");
 // It will not be included in the npm package.
 
 function main() {
-    const source = fs.readFileSync(__dirname + "/../../package.json").toString('utf-8');
+    const rootDir = path.resolve(__dirname, "../..");
+    const source = fs.readFileSync(rootDir + "/package.json").toString('utf-8');
     const sourceObj = JSON.parse(source);
     sourceObj.scripts = {};
     sourceObj.devDependencies = {};
     if (sourceObj.main.startsWith("dist/")) {
         sourceObj.main = sourceObj.main.slice(5);
     }
-    fs.writeFileSync(__dirname + "/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
-    fs.writeFileSync(__dirname + "/version.txt", Buffer.from(sourceObj.version, "utf-8") );
+    fs.writeFileSync(rootDir + "/dist/package.json", Buffer.from(JSON.stringify(sourceObj, null, 2), "utf-8") );
+    fs.writeFileSync(rootDir + "/dist/version.txt", Buffer.from(sourceObj.version, "utf-8") );
 
-    fs.copyFileSync(__dirname + "/../../.npmignore", __dirname + "/.npmignore");
+    fs.copyFileSync(rootDir + "/.npmignore", "dist/.npmignore");
 }
 
 main();
