1176ae0f795e287d7504e837e49ca3f20a135766
James Gibson <james.the.nodester@gmail.com>
2021-09-28 22:32:14 -0600

    Just to be safe.

--- scripts/publishing/prepare.cjs
+++ scripts/publishing/prepare.cjs
@@ -13,7 +13,7 @@ function main() {
     const sourceObj = JSON.parse(source);
     sourceObj.scripts = {};
     sourceObj.devDependencies = {};
-    if (sourceObj.main.startsWith('dist/')) {
+    if (sourceObj.main && sourceObj.main.startsWith('dist/')) {
         sourceObj.main = sourceObj.main.slice(5);
     }
     fs.writeFileSync(`${rootDir}/dist/package.json`, Buffer.from(JSON.stringify(sourceObj, null, 2), 'utf-8'));
