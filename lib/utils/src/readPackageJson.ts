e04f30427041cc2e28f84cb44f2d6492159d3492
James Gibson <james.the.nodester@gmail.com>
2021-09-28 22:22:34 -0600

    yep missed it

--- lib/utils/src/readPackageJson.ts
+++ lib/utils/src/readPackageJson.ts
@@ -1,12 +1,24 @@
 import { readJsonFile } from '@nrwl/workspace';
 import { existsSync } from 'fs';
 
+export type PackageJson = {
+    name: string,
+    license?: string,
+    version: string,
+    private?: boolean,
+    dependencies?: Record<string, string>,
+    devDependencies?: Record<string, string>,
+    peerDependencies?: Record<string, string>,
+    optionalDependencies?: Record<string, string>,
+    scripts?: Record<string, string>,
+}
+
 const readPackageJson = (path: string) => {
     const packagePath = `${path}/package.json`;
-    if (!existsSync(packagePath)) { return {}; }
-    return readJsonFile(packagePath);
+    if (!existsSync(packagePath)) { return {} as PackageJson; }
+    return readJsonFile(packagePath) as PackageJson;
 };
 
 export {
     readPackageJson,
-}
+};
